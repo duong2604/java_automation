c4b6a4241878a87095e27ec8526fe88e9c4193d6
duongbui <duongbuibd@gmail.com>
2025-02-07 01:15:36 +0700

    learning about Scanner

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Please enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("Please enter your height: ");
+        float height = sc.nextFloat();
+        System.out.println("Your name is " + name + ". Your age is " + age + ". Your height is " + height);
     }
 
 
